b3ed14568ffb5499c1bcf3542cbfd72b2f28e140
humberto789 <humbertovitalino1@gmail.com>
2023-04-29 00:52:40 -0300

    Pegando os dados da API e resolvendo algumas bugs nos estilos

--- src/components/SectionContainer/styles.js
+++ src/components/SectionContainer/styles.js
@@ -5,5 +5,6 @@ export const Container = styled.div`
         max-width: 120rem;
         margin: 0 auto;
         padding: ${theme.spacing.large};
+        width: 100%;
     `}
 `;
---
+++ src/templates/PageNotFound/index.jsx
@@ -0,0 +1,12 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { GridContent } from '../../components/GridContent';
+
+export const PageNotFound = () => {
+    return (
+        <GridContent
+            title="Error 404"
+            html='<p>A página que você busca não foi encontrada. <a href="/">Clique para voltar</a></p>'
+        />
+    );
+};
